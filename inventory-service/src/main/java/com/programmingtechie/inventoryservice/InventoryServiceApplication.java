3c4b233d13a1113ea0012c809a0d93c3d1d1ccb1
Narayana Reddy <nnreddy.learning@gmail.com>
2025-03-16 16:18:02 +0530

    Eureka Removal

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/InventoryServiceApplication.java
@@ -3,14 +3,14 @@ package com.programmingtechie.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.programmingtechie.inventoryservice.model.Inventory;
 import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+// @EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
